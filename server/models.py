015739143759c16669ee8fbd583b806a4bb419e4
charles <charles.angila@student.moringaschool.com>
2024-10-12 21:08:22 +0300

    "Added relationships and serialization rules to Pizza and RestaurantPizza models"

--- server/models.py
+++ server/models.py
@@ -37,8 +37,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade="all, delete-orphan")
+    restaurants = association_proxy('restaurant_pizzas','restaurant')
 
     # add serialization rules
+    
+    serializa_rules =  ('-restaurant_pizzas.pizza','-restaurants.pizzas')
 
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
@@ -51,10 +55,14 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
-
+    pizza_id =  db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable = False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable = False)
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
     # add serialization rules
-
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
     # add validation
-
+     
+     
+     
     def __repr__(self):
         return f'<RestaurantPizza ${self.price}>'
